4f1f2c7bbc68cca32099d9a8ba70e66951bd3565
Vinicius Crisol <vinicius.crisol@hotmail.com>
2021-03-18 23:07:33 -0300

    Fixing integration

--- main-api/src/shared/container/providers/MailProvider/implementations/MailProvider.ts
+++ main-api/src/shared/container/providers/MailProvider/implementations/MailProvider.ts
@@ -1,23 +1,13 @@
-import axios, { AxiosInstance } from 'axios';
+import axios from 'axios';
 import { injectable } from 'tsyringe';
 
 import IMailProvider from '../models/IMailProvider';
 
 @injectable()
 export default class MailProvider implements IMailProvider {
-  private api: AxiosInstance;
-
-  constructor() {
-    // this.api = .create({
-    //   baseURL: '',
-    // });
-  }
-
   public async sendWelcomeMail(userId: string): Promise<void> {
-    axios.post('http://localhost:8081/send/email', {
-      type: 'welcome',
-      to: `${userId}@hotmail.com`,
-      user_id: 2,
+    axios.post(`http://localhost:8081/send/email/${userId}`, {
+      template: 'welcome',
     });
   }
 }
